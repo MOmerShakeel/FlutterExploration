bd742e1addbaef0b08818309a1769cb2216a8213
Omer Shakeel <omershakeel742@gmail.com>
2024-07-30 18:45:24 +0500

    AppBar + Icons Commit

--- flutter_exploration/lib/pages/home.dart
+++ flutter_exploration/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter_svg/svg.dart";
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,19 +7,58 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "ShahKing",
-          style: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        "Amazon",
+        style: TextStyle(
+          fontSize: 24,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: const Color.fromARGB(255, 9, 80, 115),
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          width: 60,
+          margin: EdgeInsets.all(8),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Color.fromARGB(255, 234, 234, 234),
+          ),
+          child: SvgPicture.asset(
+            "assets/icons/back-button.svg",
+            height: 30,
+            width: 30,
           ),
         ),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 9, 80, 115),
-        leading: Container(),
       ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: EdgeInsets.all(8),
+            width: 40,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color.fromARGB(255, 234, 234, 234),
+            ),
+            child: SvgPicture.asset(
+              "assets/icons/options.svg",
+              height: 30,
+              width: 30,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
